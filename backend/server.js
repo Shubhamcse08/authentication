213c1403fd25e54295f4b642b5ecff821adc19ff
Shubhamcse08 <shubhamcse.eng@gmail.com>
2026-02-06 21:42:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,4 @@ app.get("/",(req,res)=>{
     res.send("API is running...")
 })
 
-const PORT= process.env.PORT || 5000;
-app.listen(PORT,()=>
-    console.log(`Server is running on ${PORT}`)
-)
+export default app;
