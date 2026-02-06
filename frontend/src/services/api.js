561d82d704136ebbc391eda6e3b4b2836d1e2f72
Shubhamcse08 <shubhamcse.eng@gmail.com>
2026-02-06 22:07:48 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://authentication-seven-henna.vercel.app"
+    baseURL: "https://authentication-seven-henna.vercel.app/api"
 })
 
 export default api;
